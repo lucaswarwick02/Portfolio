7573fdb7a27ba120a1b688b69d23278f83c966f5
FireTramp <lucaswarwick02@gmail.com>
2021-06-01 17:04:40 +0100

    Sections now fade in/out on load. Added new project: Programmable LED Mask

--- js/index.js
+++ js/index.js
@@ -6,9 +6,9 @@ const sectionTitles = [
     (<h1 id="pageTitle">Education</h1>)
 ]
 
-const sectionContents = [
+var sectionContents = [
     ( // About Me
-        <div>
+        <div id="mydiv">
             <div>
                 <p>Introduction Paragraph...</p>
             </div>
@@ -35,9 +35,50 @@ const sectionContents = [
         <div>
             <div>
                 <h2>GeoChat</h2>
-                <p>Winner for "Most Social Application" for the Highfield Hack 2021 hackatho.</p>
+                <p>Winner for "Most Social Application" for the Highfield Hack 2021 hackathon.</p>
                 <p>GeoChat uses location to provide group-chats of your closest location, allowing for the organisation of local events, advertising for local business, and being able to ask for recommendations etc. We used android studio to make it into an android app, with Google Firebase as the back-end, taking care of database issues. We consulted our friends at university the full length of the project about what features they would like to see. GeoChat was created using Android Studio and encorporated Firebase.</p>
                 <a href="https://devpost.com/software/geochat-r25ixj">Link to the GeoChat Devpost website</a>
+                <p></p>
+                <a href="https://github.com/liamhbyrne/geochat">Link to the GeoChat GitHub repository</a>
+            </div>
+            <div>
+                <h2>Programmable LED Mask</h2>
+                <p>Fully programmable LED mask built using an Arduino UNO, with multiple different 'faces' being controlled using an infared remote controller.</p>
+                <div class="row">
+                    <div class="column">
+                        <figure>
+                            <img class="mask" src="images/mask-neutral.jpg" alt="Neutral expresssion"></img>
+                            <figcaption>Neutral expresssion</figcaption>
+                        </figure>
+                    </div>
+                    <div class="column">
+                        <figure>
+                            <img class="mask" src="images/mask-happy.jpg" alt="Happy expresssion"></img>
+                            <figcaption>Happy expresssion</figcaption>
+                        </figure>
+                    </div>
+                    <div class="column">
+                        <figure>
+                            <img class="mask" src="images/mask-angry.jpg" alt="Angry expresssion"></img>
+                            <figcaption>Angry expresssion</figcaption>
+                        </figure>
+                    </div>
+                    <div class="column">
+                        <figure>
+                            <img class="mask" src="images/mask-dead.jpg" alt="X's expresssion"></img>
+                            <figcaption>X's expresssion</figcaption>
+                        </figure>
+                    </div>
+                    <div class="column">
+                        <figure>
+                            <img class="mask" src="images/mask-disco.jpg" alt="Disco expresssion"></img>
+                            <figcaption>Disco expresssion</figcaption>
+                        </figure>
+                    </div>
+                </div>
+                <p>The mask is powered by a single a 5V portable charger plugged into the Arduino UNO's USB-A port. The wiring for the infared controller is 1 wire into the Arduino's 3V supply, another into a data port and the last into one of the 2 GND ports.</p>
+                <p>The LEDs are plugged into a breadboard in parllel and each LED strip is wired into seperate data ports in order to recieve different data packets. From there, whenever a button is pressed on the IR controller it the script will run one of a multitude of functions which will change the LED configuration.</p>
+                <a href="https://github.com/lucaswarwick02/programmable-led-mask">Link to the Mask's GitHub repository</a>
             </div>
         </div>
     ),
@@ -94,18 +135,14 @@ function updatePage (element) {
     else {
         id = 0;
     }
-    updateSectionTitle();
-    updateSectionContents();
+    fadeUpdate();
 }
 
-function updateSectionTitle () {
-    console.log("Updating Section Title");
-    ReactDOM.render(sectionTitles[id], document.getElementById("sectionTitle"));
-}
-
-function updateSectionContents () {
-    console.log("Updating Section Contents");
+async function fadeUpdate () {
+    await $('.content').fadeOut(150).promise();
     ReactDOM.render(sectionContents[id], document.getElementById("sectionContents"));
+    ReactDOM.render(sectionTitles[id], document.getElementById("sectionTitle"));
+    await $('.content').fadeIn(150).promise();
 }
 
 window.onload = updatePage;
